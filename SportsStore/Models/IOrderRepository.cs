02b0aec31161fb91559f6683f944d9b8020783a2
Nakaw <nakawyoung@gmail.com>
2022-12-14 14:34:58 -0500

    second commit

---
+++ SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,6 @@
+namespace SportsStore.Models{
+    public interface IOrderRepository{
+        IQueryable<Order>Orders{get;}
+        void SaveOrder(Order order);
+    }
+}
---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,7 @@
+namespace SportsStore.Models.ViewModels{
+    public class ProductsListViewModel{
+        public IEnumerable<Product>Products{get;set;}=Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo{get;set;}=new();
+        public string? CurrentCategory{get;set;}
+    }
+}
