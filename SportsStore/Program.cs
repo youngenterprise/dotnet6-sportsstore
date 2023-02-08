96c50d3949715e76ad60c78a13a4c85318b38649
Nakaw <nakawyoung@gmail.com>
2023-02-08 13:37:02 -0500

    Added admin login

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,11 +20,26 @@ builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole> ().AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
 var app = builder.Build();
+
+if(app.Environment.IsProduction()){
+    app.UseExceptionHandler("/error");
+}
+
+app.UseRequestLocalization(opts =>{
+    opts.AddSupportedCultures("en-US").AddSupportedUICultures("en-US").SetDefaultCulture("en-US");
+});
 
 //app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
 app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new {Controller = "Home", action = "index"});
  
@@ -41,5 +57,6 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/admin/{*catchall}","/Admin/Index");
 
 SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 
 app.Run();
